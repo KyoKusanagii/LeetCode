4fa5a7cef9f6a3ad2dff1c7cf36e360ec966c1cd
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-07-17 23:03:46 +0800

    無聊玩玩

--- src/ReverseStringII.java
+++ src/ReverseStringII.java
@@ -1,42 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseStringII {
     public String reverseStr(String s, int k) { //k==3
         char[] c = s.toCharArray();
-        boolean reverse = true;
-        StringBuilder builder = new StringBuilder();
-        String ans = "";
-        int a = 0;
-        for (int i = 1; i <= (c.length / (2*k)); i++) { //切幾段 0~5 是一段
-            StringBuilder builder1 = new StringBuilder();
-            for(int j = (i-1)*k ; j<=i*k-1;j++){  // 0~2、3~5
-                builder1.append(c[j]);
-                a = j;
-            }
-            if(reverse){
-                builder1 = builder1.reverse();
-                reverse = false;
-            }
-            builder.append(builder1);
+        List<Character> list = new ArrayList<>();
+        List<Character> reverlist = new ArrayList<>();
+        for (char c1 : c) {
+            list.add(c1);
         }
-        if(c.length%(2*k)==0){
-            ans = builder.toString();
-        }else{
-            int x = 1;
-            boolean reverse2 = true;
-            for(int j = a + 1;j<c.length;j++){
-                StringBuilder builder1 = new StringBuilder();
-                if(x<=k){
-                    builder1.append(c[j]);
-                    x++;
-                }else if(x>k){
-                    if(reverse2){
-                        builder.append(builder1.reverse());
-                        reverse2 = false;
+
+        if (list.size() >= k) {
+            for (int i = 0; i < list.size() / 2*k; i++) {
+                StringBuilder builder = new StringBuilder();
+                for (int j = i * 2*k; j <= i * 2*k + (2*k - 1); j++) {
+                    if(j==list.size()){
+                        break;
                     }
-                    builder.append(c[j]);
+                    builder.append(list.get(j));
+                    if (j % k == k-1) {
+                        builder = builder.reverse();
+                    }
+                }
+                for (int a = 0; a < builder.length(); a++) {
+                    reverlist.add(builder.charAt(a));
                 }
             }
-            ans = builder.toString();
+            int x = list.size() % (2 * k);
+            if (x < k) {
+                for (int i = list.size() - 1; i >= list.size() - x; i--) {
+                    reverlist.add(list.get(i));
+                }
+            } else if (k <= x && x < (2*k)){
+                StringBuilder builder1 = new StringBuilder();
+                boolean firstReverse = false;
+                List<Character> leftList = list.subList(list.size()-x,list.size());
+                for (int i = 0;i<leftList.size(); i++) {
+                    if (i < k) {
+                        builder1.append(leftList.get(i));
+                    } else if(i>=k){
+                        if (!firstReverse) {
+                            builder1 = builder1.reverse();
+                            firstReverse = true;
+                        }
+                        builder1.append(leftList.get(i));
+                    }
+                }
+                for(int z=0;z<builder1.length();z++){
+                    reverlist.add(builder1.charAt(z));
+                }
+            }
+        } else if (k > list.size()) {
+            StringBuilder builder = new StringBuilder(s).reverse();
+            for (int i = 0; i < builder.length(); i++) {
+                reverlist.add(builder.charAt(i));
+            }
         }
-        return ans;
+        StringBuilder builder = new StringBuilder();
+        for (char c2 : reverlist) {
+            builder.append(c2);
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        ReverseStringII r = new ReverseStringII();
+        System.out.println(r.reverseStr("abcdefg",2));
+
     }
 }
